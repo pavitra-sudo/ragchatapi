b154a634b75b807f0dc4a989357e58cb3727131a
pavitra-sudo <213853274+pavitra-sudo@users.noreply.github.com>
2026-05-10 17:07:27 +0530

    embed

---
+++ app/rag/embeddings.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+
+from app.config import settings
+
+
+client = OpenAI(
+    api_key=settings.OPENAI_API_KEY
+)
+
+
+def generate_embedding(text: str):
+
+    response = client.embeddings.create(
+        model="text-embedding-3-small",
+        input=text
+    )
+
+    embedding = response.data[0].embedding
+
+    return embedding
